93f24ab07de8ae6a5062bcb2e4adbb817b7961cf
Your name here <elia.orsini@hotmail.com>
2025-08-06 17:51:58 +0800

    chore: linting

--- src/components/Footer/FooterWrapper.tsx
+++ src/components/Footer/FooterWrapper.tsx
@@ -1,11 +1,9 @@
-import { fetchOptions } from "@/constants/constants";
-import { client } from "@/sanity/client";
 import Footer from "./Footer";
 
-const INFORMATION = `*[_type == "information"][]{statement, instagram, email}`;
+// const INFORMATION = `*[_type == "information"][]{statement, instagram, email}`;
 
 export default async function FooterWrapper() {
-  const information = await client.fetch<any[]>(INFORMATION, {}, fetchOptions);
+  // const information = await client.fetch<any[]>(INFORMATION, {}, fetchOptions);
 
   return <Footer />;
 }
