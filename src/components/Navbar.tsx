e98ef02b0742405f926fb8435cd6f820bc2bae7a
Your name here <elia.orsini@hotmail.com>
2025-08-15 11:35:08 +0800

    chore: linting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { TransitionLink } from "./TransitionLink";
