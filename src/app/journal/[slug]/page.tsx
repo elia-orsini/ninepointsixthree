a5a52708af3294d04827d6b52c487ac5aa1ab5b2
Your name here <elia.orsini@hotmail.com>
2025-08-20 20:59:35 +0200

    feat: use JSON_LD for single article page

--- src/app/journal/[slug]/page.tsx
+++ src/app/journal/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { client } from "@/sanity/client";
 import JournalArticle from "@/components/Journal/JournalArticle";
 import { notFound } from "next/navigation";
+import { urlFor } from "@/sanity/urlFor";
 
 const journalPostQuery = `*[_type == "journal" && slug.current == $slug][0] {
   _id,
@@ -52,6 +53,8 @@ interface JournalPost {
   mainImage: any;
   publishedAt: string;
   body: any;
+  excerpt?: string;
+  series?: string;
 }
 
 interface PageProps {
@@ -68,11 +71,48 @@ export default async function JournalPostPage({ params }: PageProps) {
     notFound();
   }
 
+  // Generate JSON-LD structured data
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "headline": post.title,
+    "description": post.excerpt || post.title,
+    "image": post.mainImage ? urlFor(post.mainImage).width(1200).height(630).url() : undefined,
+    "datePublished": post.publishedAt,
+    "dateModified": post.publishedAt,
+    "author": {
+      "@type": "Organization",
+      "name": "Nine Point Six Three",
+      "url": "https://www.ninepointsixthree.co"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Nine Point Six Three",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://www.ninepointsixthree.co/963-logo.svg"
+      }
+    },
+    "mainEntityOfPage": {
+      "@type": "WebPage",
+      "@id": `https://www.ninepointsixthree.co/journal/${post.slug.current}`
+    },
+    "url": `https://www.ninepointsixthree.co/journal/${post.slug.current}`,
+    "articleSection": post.series || "Journal",
+    "wordCount": post.body ? JSON.stringify(post.body).length : undefined,
+  };
+
   return (
-    <main className="min-h-screen">
-      <div className="relative z-20 mx-auto mb-[57px] mt-[90px] w-[calc(100vw-48px)] animate-fadeUp space-y-[18px] sm:w-[490px] lg:mt-[35px]">
-        <JournalArticle key={post._id} post={post} />
-      </div>
-    </main>
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <main className="min-h-screen">
+        <div className="relative z-20 mx-auto mb-[57px] mt-[90px] w-[calc(100vw-48px)] animate-fadeUp space-y-[18px] sm:w-[490px] lg:mt-[35px]">
+          <JournalArticle key={post._id} post={post} />
+        </div>
+      </main>
+    </>
   );
 }
