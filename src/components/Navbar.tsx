8c61a036cb00da3880705f17fd242cfa6723e1b8
Your name here <elia.orsini@hotmail.com>
2025-08-29 09:29:00 +0100

    fix: line height

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -83,12 +83,12 @@ export default function Navbar({ information }: { information: any }) {
             />
           </svg>
         </button>
-        <div className="px-[36px] pb-[18px] leading-[150%]">
-          <p className="leading-[150%]">{information?.statement}</p>
+        <div className="px-[36px] pb-[18px] leading-[135%]">
+          <p className="leading-[135%]">{information?.statement}</p>
 
           <div className="mt-[18px] flex flex-col gap-x-[90px] sm:flex-row">
             <div className="flex flex-col gap-y-[18px]">
-              <div className="flex flex-col leading-[150%]">
+              <div className="flex flex-col leading-[135%]">
                 <p className="text-[#BCBCBC]">Services</p>
                 {services.length > 0 ? (
                   services.map((svc, idx) => <p key={`svc-${idx}`}>{svc}</p>)
